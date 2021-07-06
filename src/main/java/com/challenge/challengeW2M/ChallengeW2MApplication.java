bfa23f1b84ffc68bb04c465d46fde11e3f16cce8
adrian <apenzone@technisys.com>
2021-07-06 20:51:18 -0300

    Initial archetype

---
+++ src/main/java/com/challenge/challengeW2M/ChallengeW2MApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.challengeW2M;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeW2MApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeW2MApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/challengeW2M/ChallengeW2MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.challengeW2M;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeW2MApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
